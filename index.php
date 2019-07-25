7db780204b751d82943dc50bfd708faafa314dfa
briandlt <38438974+briandlt@users.noreply.github.com>
2019-07-25 13:30:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-    session_start();
-    require_once './models/conexion.php';
-    require_once './models/home.model.php';
-    require_once './controllers/home.controller.php';
+//     session_start();
+//     require_once './models/conexion.php';
+//     require_once './models/home.model.php';
+//     require_once './controllers/home.controller.php';
+echo "hola mundo";
 
 ?>
 
